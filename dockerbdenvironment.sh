936dc6ed3f710f994663e0e12ecc013974000d44
José Manuel Alvarez <97788933+profjosemanuel@users.noreply.github.com>
2025-06-05 17:32:31 +0200

    Update dockerbdenvironment.sh

--- dockerbdenvironment.sh
+++ dockerbdenvironment.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 docker run --name mariadb-container -d \
-    -e MYSQL_ROOT_PASSWORD=my-secret-pw \
-    -e MYSQL_DATABASE=mydatabase \
+    -e MYSQL_ROOT_PASSWORD=secret \
+    -e MYSQL_DATABASE=wordpress \
+    -e MYSQL_USER=manager \
     -p 3306:3306 \
     mariadb:latest
